8f2afda72e3db53fc188c1049ad3bf74bb01bc6a
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-05-03 17:28:34 +0330

    Make DarkModeButton component to handle dark mode

--- src/Components/FAQList.tsx
+++ src/Components/FAQList.tsx
@@ -1,7 +1,9 @@
 //! Components
+import DarkModeButton from "./DarkModeButton";
 import FAQItem from "./FAQItem";
 
 function FAQList() {
+
   return (
     <div className="max-w-3xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <div className="flex flex-col sm:flex-row justify-between items-center mb-8 gap-4">
@@ -13,11 +15,10 @@ function FAQList() {
             <i className="bx bx-collapse-alt text-lg"></i>
             <span>Expand All</span>
           </button>
-          <button className="m-10 h-10 flex items-center justify-center rounded-full text-gray-700 bg-gradient-to-r from-gray-200 to-gray-300 hover:shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 transition-all duration-300 cursor-pointer">
-            <i className="bx bx-moon text-xl"></i>
-          </button>
+          <DarkModeButton />
         </div>
       </div>
+      <FAQItem />
     </div>
   );
 }
