0775979bc1d6f998781df7eff340785e1ea05680
R.B <110852196+MRFAKE2002@users.noreply.github.com>
2025-05-03 12:47:44 +0330

    Make Header and FAQList components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-import Image from "next/image";
+//! Components
+import FAQList from "@/Components/FAQList";
+import Header from "@/Components/Header";
 
 export default function Home() {
   return (
-    <div></div>
+    <div className="max-h-screen bg-gradient-to-br from-gray-50 via-gray-100 to-gray-50">
+      <Header />
+      <FAQList />
+    </div>
   );
 }
